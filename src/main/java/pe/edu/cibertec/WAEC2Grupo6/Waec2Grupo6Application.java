175d269569bd617209c2eb9ccba128fba60e3f69
27jorg16 <jorgevaldivieso-@hotmail.com>
2024-04-26 18:06:32 -0500

    inicio

---
+++ src/main/java/pe/edu/cibertec/WAEC2Grupo6/Waec2Grupo6Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2Grupo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waec2Grupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Waec2Grupo6Application.class, args);
+	}
+
+}
