17ce0996bc6baac4d5ceff53b553ec029d9851b3
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-04-26 21:50:57 -0500

    Revert "Demo part4"

--- src/main/java/pe/edu/cibertec/WAEC2Grupo6/model/dto/response/CategoriaResponse.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.cibertec.WAEC2Grupo6.model.dto.response;
-import lombok.Builder;
-import lombok.Data;
-@Data
-@Builder
-public class CategoriaResponse {
-    private Boolean respuesta;
-    private String mensaje;
-}
